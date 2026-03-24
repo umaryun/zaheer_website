381addccef45b1cb77c79ad99900a79dfa7e4de2
Umar <umaryunusa443@gmail.com>
2026-03-24 04:25:43 +0100

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <div className="flex-shrink-0">
+          <div className="shrink-0">
             {/* <img
               src="/logo.png"
               alt="Zaheer Contracts"
